dd90df2887b2a0ce1fbb3d06a157c2c71074f21e
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2022-11-06 11:09:55 -0500

    v1

---
+++ README.md
@@ -0,0 +1,36 @@
+# Gradescope Downloader
+
+### Connor Leggett
+### 11/06/2022
+
+This code is meant to download submissions from Gradescope, sort them by TF, and create a homepage for each TF to provide easier access to homepage submissions.
+
+## Setup
+
+1. Configure Environment
+   1. Clone/Fork this repository, then navigate to the appropriate directory
+   2. Use `conda env create -f environment.yml` to create the new environment
+   3. Activate your new environment with `conda activate [environment_name]`
+2. Provide Environment Variables: Create a file titled `.env` and fill it using the template below:
+```
+USERNAME=[Your Gradescope Username]
+PASS=[Your Gradescope Password]
+COURSE_NO=[The Gradescope Course Number]
+ASSIGNMENT_NO=[The Gradescope Assignment Number]
+```
+3. Set start time. To save time, this program will not download the same submission twice, so it keeps track of the submission time of the most recent submission. When downloading for the first time, edit `last_sweep.txt` to a date from last year
+
+## Usage
+
+Run the program by navigating to the appropriate directory and running
+```bash
+python main.py
+```
+
+## Output
+
+When the program finishes running, there will be a folder for each TF in the `downloads` directory, which contains a directory for each of their students' projects, and an `index.html` file which will allow them to easily navigate to most of their students websites. A list of TFs whos folders have been updated will be printed to the terminal, which will be helpful in managing late submissions, as only those TFs need to have their folders updated.
+
+## Distribution
+
+The best way I've found in the past for distributing these files is by uploading these folders to Google Drive and sharing them with each TF. It would be an interesting future task to automate this process.
--- main.py
+++ main.py
@@ -13,12 +13,14 @@ PASS = os.getenv("PASS")
 COURSE_NO = os.getenv("COURSE_NO")
 ASSIGNMENT_NO = os.getenv("ASSIGNMENT_NO")
 BASE_URL = "https://www.gradescope.com"
+ASSIGNMENT_URL = f"{BASE_URL}/courses/{COURSE_NO}/assignments/{ASSIGNMENT_NO}"
 
 # Get most recent file read date:
 with open("last_sweep.txt", "r") as f:
     last_sweep = f.read()
 
 def open_browser():
+    """Sets up chromedriver and opens browser"""
     options = webdriver.ChromeOptions()
     prefs = {
         "download.default_directory": f"{os.getcwd()}/downloads"
@@ -28,6 +30,7 @@ def open_browser():
     return driver
 
 def login(driver):
+    """Logs into gradescope with username and password provided in .env file"""
     login_url = f"{BASE_URL}/login"
     driver.get(login_url)
     username_input = driver.find_element("id", "session_email")
@@ -38,7 +41,8 @@ def login(driver):
     submit.click()
 
 def download_submission_csv(driver):
-    download_url = f"{BASE_URL}/courses/{COURSE_NO}/assignments/{ASSIGNMENT_NO}/scores.csv"
+    """Downloads csv with info on submissions"""
+    download_url = f"{ASSIGNMENT_URL}/scores.csv"
     driver.get(download_url)
     try:
         os.remove(f"{os.getcwd()}/downloads/csv/scores.csv")
@@ -56,6 +60,7 @@ def download_submission_csv(driver):
             break
 
 def get_tfs_dict():
+    """Uses CSV to return dictionary in the form tf_name: list of students"""
     tfs = {}
     with open("downloads/csv/scores.csv", "r") as f:
         reader = csv.DictReader(f)
@@ -69,7 +74,7 @@ def get_tfs_dict():
                     most_recent = row["Submission Time"]
                 tfs[tf].append({
                     "student": row["Name"],
-                    "link": f"{BASE_URL}/courses/{COURSE_NO}/assignments/{ASSIGNMENT_NO}/submissions/{row    ['Submission ID']}.zip",
+                    "link": f"{ASSIGNMENT_URL}/submissions/{row['Submission ID']}.zip",
                     "submission_id": row["Submission ID"],
                     "sub_time": row["Submission Time"]
                 })
@@ -79,11 +84,13 @@ def get_tfs_dict():
     return tfs
 
 def setup_tf_folders(tfs):
+    """Creates a new folder for each TF in the downloads folder"""
     for tf in tfs:
         if not os.path.isdir(f"downloads/{tf}"):
             os.makedirs(f"downloads/{tf}")
 
 def manage_one_sub(tf, student, link, sub_id, driver):
+    """Downloads one submission, unzips it, and places it in the correct file"""
     driver.get(link)
     try:
         alert = driver.find_element("css selector", ".l-header > div")
@@ -99,20 +106,27 @@ def manage_one_sub(tf, student, link, sub_id, driver):
     return True
 
 def download_subs(tfs, driver):
+    """Download all submissions, keeping track of which are too big"""
     too_big = set()
+    updated_tfs = set()
     for tf, students in tfs.items():
         for student in students:
             if student["sub_time"] > last_sweep:
                 print(f"Downloading Submission {student['submission_id']}")
+                updated_tfs.add(tf)
                 if not manage_one_sub(tf, student["student"], student["link"], student["submission_id"], driver):
                     too_big.add(student["student"])
+    print("Changes made to:")
+    for tf in updated_tfs:
+        print(f"  - {tf}")
     return too_big
 
 def create_homepage(tfs, too_big):
+    """Creates an HTML file with links to students' index.html pages"""
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("template.html")
     for tf, students in tfs.items():
-        students = {s["student"]: f"./{s['student']}/index.html" if s["student"] not in too_big else f"{BASE_URL}/courses/{COURSE_NO}/assignments/{ASSIGNMENT_NO}/submissions/{s['submission_id']}" for s in students}
+        students = {s["student"]: f"./{s['student']}/index.html" if s["student"] not in too_big else f"{ASSIGNMENT_URL}/submissions/{s['submission_id']}" for s in students}
         content = template.render(
             tf=tf,
             students=students
